339977d1a60f5f7f83228aec7c3acba4bb3f82d3
maysharmaaa <56557130+maysharmaaa@users.noreply.github.com>
2022-07-30 02:08:43 +0530

    Update HelloWorld.java

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
 	public static void main(String[] args) {
 		long milli = System.currentTimeMillis();
-		System.out.println(milli + "  ----- We are inside a Hello World Java Program");
+		System.out.println(milli + "  ----- *Updated* We are inside a Hello World Java Program");
 	}
 }
