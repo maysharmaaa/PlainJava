7988d03699fae92360b36f1d9c3c9c68f81bd0d2
maysharmaaa <maysharmaaa@gmail.com>
2022-07-30 01:15:14 +0530

    Hello World

---
+++ src/HelloWorld.java
@@ -0,0 +1,6 @@
+public class HelloWorld {
+	public static void main(String[] args) {
+		long milli = System.currentTimeMillis();
+		System.out.println(milli + "  ----- We are inside a Hello World Java Program");
+	}
+}
